97346a8b4e610f47063f53fcf07cc3dc47c62b97
4ebula <4ebula@gmail.com>
2024-07-07 21:18:59 +0200

    feat: implement spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './spinner.scss';
+
+export class SpinnerComponent extends React.PureComponent {
+  render(): React.ReactNode {
+    return <div className="spinner"></div>;
+  }
+}
