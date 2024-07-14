e2724d9ca4d60a9e9faca2f3f9df7b5575609fb6
4ebula <4ebula@gmail.com>
2024-07-14 19:06:16 +0200

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+// import { App } from './App.tsx';
 import './index.scss';
 import { ErrorBoundary } from './components/error-boundary/error-boundary.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+
     <ErrorBoundary>
-      <App />
+    <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>,
 );
