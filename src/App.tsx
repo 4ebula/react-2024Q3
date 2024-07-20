302841e44b7e882140490d52a82bdb35f7d9b643
4ebula <4ebula@gmail.com>
2024-07-20 16:48:30 +0200

    feat: implement custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 import { SearchComponent } from './components/search/search';
 import { ResultsComponent } from './components/results/result';
 import { ErrorBtnComponent } from './components/error-btn/error-btn';
 import './App.scss';
+import { useStorage } from './hooks/storage.hook';
 
 export function App(): ReactNode {
   const searchDefaultKey = 'query';
 
-  const [query, setQuery] = useState(localStorage.getItem(searchDefaultKey));
-
-  const searchChangeCb = (value: string): void => {
-    const query = value.trim().toLowerCase();
-    setQuery(query);
-    localStorage.setItem(searchDefaultKey, query);
-  };
+  const [query, setQuery] = useStorage(searchDefaultKey);
 
   return (
     <>
@@ -21,7 +16,7 @@ export function App(): ReactNode {
         <ErrorBtnComponent></ErrorBtnComponent>
         <SearchComponent
           query={query}
-          changeSearch={searchChangeCb}
+          changeSearch={setQuery}
         ></SearchComponent>
       </div>
       <ResultsComponent query={query}></ResultsComponent>
